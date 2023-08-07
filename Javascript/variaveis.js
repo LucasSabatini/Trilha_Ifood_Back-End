b59f1f5bfe6c2f3cfdbccdbe74aacaf8001add40
lucassabatini <lucas.frsabatini@gmail.com>
2023-08-07 14:42:22 -0300

    Aula 2 - Variáveis

---
+++ Javascript/variaveis.js
@@ -0,0 +1,26 @@
+//VARIÁVEIS
+let idade = 29; //Criando a primeira variável
+console.log(idade);
+
+//Tipos de variáveis
+idade = 30; //Number
+let numero = 30; //Number
+let altura = 1.71; //Number
+let nome = "Lucas"; //String
+let estudando = true; //Boolean
+
+console.log(numero, typeof numero);
+console.log(altura, typeof altura);
+console.log(nome, typeof nome);
+console.log(estudando, typeof estudando);
+
+let outra;
+console.log(outra);
+
+let nula = null;
+console.log(nula);
+
+var variavel = 30; //Não utilizar mais 'var' e sim 'let'
+
+const constante = 100;
+console.log(constante);
